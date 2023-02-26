ae7c51a9ca57c9a224309ee0451997693d616502
CapetMaiden <iemorhokpor@gmail.com>
2023-02-26 15:37:01 -0500

    restored components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <BudgetForm />
+                        <Budget />
                     </div>
                     <div className='col-sm'>
                         <RemainingBudget />
